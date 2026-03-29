5052b1a650e3294e764eea71be8ee2db86e56488
hlonrno <moony25@proton.me>
2026-03-29 19:37:26 +0300

    more fixes, also moving where looking

--- src/gfx/camera.c
+++ src/gfx/camera.c
@@ -1,6 +1,9 @@
 #include "camera.h"
 #include <math.h>
 
+#define PI 3.141565f
+void rotate(vec3 vec, vec3 rot);
+
 void camera_init(camera_t *camera, vec3 origin, vec3 target, vec3 up, vec3 front) {
     memset(camera, 0, sizeof(*camera));
     memcpy(camera->origin, origin, sizeof(camera->origin));
@@ -16,6 +19,7 @@ void camera_perspective_init(camera_t *camera, float fov, float aspect, float ne
     camera->perspective.farZ = farZ;
 }
 
+vec3 scratch_buf;
 void camera_move(camera_t *camera, enum CameraDirection direction) {
     vec3 offset = {0};
     switch (direction) {
@@ -23,17 +27,36 @@ void camera_move(camera_t *camera, enum CameraDirection direction) {
             offset[2] = -1;
             break;
         case CAMERA_DIRECTION_LEFT:
-            offset[0] = -1;
+            offset[0] = 1;
             break;
         case CAMERA_DIRECTION_BACK:
             offset[2] = 1;
             break;
         case CAMERA_DIRECTION_RIGHT:
-            offset[0] = 1;
+            offset[0] = -1;
     }
 
+    rotate(offset, camera->rotation);
     glm_vec3_add(camera->origin, offset, camera->origin);
     glm_vec3_add(camera->target, offset, camera->target);
+}
+
+vec3 pmouse = (vec3){0, 0, 0};
+void camera_mouse_cb(camera_t *camera, double mouseX, double mouseY) {
+    camera->rotation[0] -= (mouseX - pmouse[0]) * PI / 180;
+    camera->rotation[1] += (mouseY - pmouse[1]) * PI / 180;
+    if (camera->rotation[1] < PI / 2) camera->rotation[1] = PI / 2;
+    if (camera->rotation[1] > 1.5f * PI) camera->rotation[1] = 1.5f * PI;
+
+    // scratch = rotate(front, rotation)
+    memcpy(scratch_buf, camera->front, sizeof(vec3));
+    rotate(scratch_buf, camera->rotation);
+    // scratch += origin;
+    glm_vec3_add(scratch_buf, camera->origin, scratch_buf);
+    // target = scratch;
+    memcpy(camera->target, scratch_buf, sizeof(vec3));
+    pmouse[0] = mouseX;
+    pmouse[1] = mouseY;
 }
 
 void rotate(vec3 vec, vec3 rot) {
@@ -49,25 +72,5 @@ void rotate(vec3 vec, vec3 rot) {
     vec[0] = (float)(x * (ca * cb) + y * (ca * sb * sg - sa * cg) + z * (ca * sb * cg + sa * sg));
     vec[1] = (float)(x * (sa * cb) + y * (sa * sb * sg + ca * cg) + z * (sa * sb * cg - ca * sg));
     vec[2] = (float)(x * (-sb) + y * (cb * sg) + z * (cb * cg));
-}
-
-#define PI 3.141565f
-vec3 scratch_buf;
-vec3 pmouse = (vec3){0, 0, 0};
-void camera_mouse_cb(camera_t *camera, double mouseX, double mouseY) {
-    camera->rotation[0] += (mouseX - pmouse[0]) * PI / 180;
-    camera->rotation[1] += (mouseY - pmouse[1]) * PI / 180;
-    if (camera->rotation[1] < PI / 2) camera->rotation[1] = PI / 2;
-    if (camera->rotation[1] > 1.5f * PI) camera->rotation[1] = 1.5f * PI;
-
-    // scratch = rotate(front, rotation)
-    memcpy(scratch_buf, camera->front, sizeof(vec3));
-    rotate(scratch_buf, camera->rotation);
-    // scratch += origin;
-    glm_vec3_add(scratch_buf, camera->origin, scratch_buf);
-    // target = scratch;
-    memcpy(camera->target, scratch_buf, sizeof(vec3));
-    pmouse[0] = mouseX;
-    pmouse[1] = mouseY;
 }
 #undef PI
